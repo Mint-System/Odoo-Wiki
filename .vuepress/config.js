6e93f0087268b1a409d32a038225619d4f9ee61e
Janik von Rotz <login@janikvonrotz.ch>
2020-11-22 21:09:40 +0100

    auto sidebar

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -2,6 +2,9 @@
 module.exports = {
     title: 'Odoo Handbuch',
     description: 'Odoo Handbuch',
+    themeConfig: {
+        sidebar: 'auto'
+    },
     plugins: [
         'vuepress-plugin-mermaidjs',
         'plausible-analytics',
