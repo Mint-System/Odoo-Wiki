1a78320b6106c3a27c86f280b5b584ffd65fea02
Janik von Rotz <login@janikvonrotz.ch>
2020-11-22 21:00:17 +0100

    mermaid

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -3,6 +3,7 @@ module.exports = {
     title: 'Odoo Handbuch',
     description: 'Odoo Handbuch',
     plugins: [
+        'vuepress-plugin-mermaidjs',
         'plausible-analytics',
         '@vuepress/back-to-top'
     ]
