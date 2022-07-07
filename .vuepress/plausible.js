6909a0bd335ab4dda618dfc7c29afe2267183410
janikvonrotz <contact@janikvonrotz.ch>
2022-07-07 23:39:55 +0200

    remove log

--- .vuepress/plausible.js
+++ .vuepress/plausible.js
@@ -3,7 +3,6 @@ const { path } = require('@vuepress/utils');
 const name = '@mint-system/plugin-plausible'
 
 const plausiblePlugin = ({ domain }) => {
-    console.log(domain)
     return {
         name,
         clientConfigFile: path.resolve(__dirname, 'client.js'),
