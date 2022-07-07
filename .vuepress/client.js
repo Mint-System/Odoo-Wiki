bcb1a365eb77a1f1dd5f8a633412cb976a937134
janikvonrotz <contact@janikvonrotz.ch>
2022-07-07 23:55:36 +0200

    skip ssr rendering

--- .vuepress/client.js
+++ .vuepress/client.js
@@ -3,13 +3,13 @@ import { defineClientConfig } from '@vuepress/client'
 const { domain = undefined } = __PLAUSIBLE_OPTIONS__
 
 export default defineClientConfig({
-  enhance({ router }) {
-
-    var script = document.createElement('script')
-    script.defer = true
-    script.dataset.domain = domain
-    script.src = "https://plausible.io/js/script.js"
-    document.getElementsByTagName('head')[0].appendChild(script)
-
-  },
+	enhance({ router }) {
+		if (!__VUEPRESS_SSR__) {
+			var script = document.createElement('script')
+			script.defer = true
+			script.dataset.domain = domain
+			script.src = "https://plausible.io/js/script.js"
+			document.getElementsByTagName('head')[0].appendChild(script)
+		}
+	}
 })
