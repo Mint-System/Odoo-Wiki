eb4d74f81d43d1e91072632299659748c44a4178
janikvonrotz <contact@janikvonrotz.ch>
2022-11-10 14:47:11 +0100

    vault backup: 2022-11-10 14:47:11

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -29,7 +29,7 @@ module.exports = {
     }),
     plugins: [
         searchPlugin({
-            maxSuggestions: 10
+            maxSuggestions: 20
         }),
         plausiblePlugin({
             'domain': 'odoo-wiki.org'
