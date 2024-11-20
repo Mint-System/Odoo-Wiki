0458b3cda9b44ae40382c7d2adfd36b9aba0a37f
Janik von Rotz <login@janikvonrotz.ch>
2024-11-20 09:17:16 +0100

    featy: theme light plus

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -47,6 +47,7 @@ export default defineUserConfig({
         }),
         mermaidPlugin(),
         shikiPlugin({
+            theme: 'light-plus',
             langs: ['bash', 'yml', 'yaml', 'json', 'css', 'html', 'xml', 'groovy', 'py', 'python', 'sql', 'powershell', 'txt', 'csv'],
         }),
     ],
